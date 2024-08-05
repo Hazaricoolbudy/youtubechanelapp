3c3903cda912d72f29f3f4e764eb643b35bea607
hazari <hazarisah48@gmail.com>
2024-08-05 12:24:05 +0530

    database connection string attached sucessfully

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+console.log(process.env.DATABASE_URI);
+const connectDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.DATABASE_URI}/${DB_NAME}`
+    );
+    console.log(
+      `Database Connected !! on host ${connectionInstance.connection.host} `
+    );
+  } catch (error) {
+    console.log("Mongodb Error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
