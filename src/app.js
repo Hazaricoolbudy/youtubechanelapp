2ea3309f5324934ae3dccebbaddf02dc8649bb88
hazari sah <hazarisah48@gmail.com>
2024-08-06 21:08:17 +0530

    working on registraion controller

--- src/app.js
+++ src/app.js
@@ -12,5 +12,11 @@ app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ limit: "15kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+// route declerations
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
